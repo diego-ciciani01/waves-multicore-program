5b9dfb755b925a5ba236ca7609c5b3b14890941a
diego-ciciani01 <diego.ciciani@gmail.com>
2026-01-30 16:08:55 +0100

    MPI + OMP and studen.mk

--- student.mk
+++ student.mk
@@ -1,21 +1,33 @@
 # --- Execution Parameters ---
-export OMP_NUM_THREADS=4
-MPI_PROCS=8
+export OMP_NUM_THREADS=8
+MPI_PROCS=4 #8
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
-export ONE_BIG_FILE=
+#export OMP_WAIT_POLICY=active
+export OMP_DYNAMIC=false
+export IS_PROFILING=false
 # --- MPI Run Flags ---
 MPIRUN_FLAGS = -np $(MPI_PROCS) \
-               --bind-to core
+			   --map-by socket:PE=8 \
+			   --bind-to core
+
 # --- Compiler Flags ---
 # Flags for MPI+OpenMP code
 # Uncomment and add extra flags if you need them
-#MPI_OMP_EXTRA_CFLAGS =
-MPI_OMP_EXTRA_LIBS = -march=native \
-					 -ffast-math
+MPI_OMP_EXTRA_CFLAGS = -DIS_PROFILING=$(IS_PROFILING)
+MPI_OMP_EXTRA_LIBS =  -fopenmp \
+                       -O3 \
+                       -march=native \
+                       -mtune=native \
+                       -mfma \
+                       -mavx2 \
+                       -fno-math-errno \
+                       -fno-trapping-math \
+                       -fno-signaling-nans \
+                       -funroll-loops \
+					   -ffinite-math-only
 
 # Flags for CUDA code
 # Uncomment and add extra flags if you need them
-# CUDA_EXTRA_CFLAGS = -gencode arch=compute_52,code=sm_52 # allow run on gtx 970 
-CUDA_EXTRA_CFLAGS = -O3 
+CUDA_EXTRA_CFLAGS = -03
 #CUDA_EXTRA_LIBS =
